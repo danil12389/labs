310bda1ba0429a7d6ff3b431c4b5f8e46f052ccd
danil12389 <akverrr@gmail.com>
2021-12-13 21:37:42 +0300

    доделал 1.8, 2.10, 3.1, однако в 1.8 все еще не уверен

--- Laba3/Laba3/Program.cs
+++ Laba3/Laba3/Program.cs
@@ -12,7 +12,7 @@ namespace Laba3
         //третий вариант
         static void Main(string[] args)
         {
-            Console.WriteLine("Какую задачу хотите посмотреть? Введите номер в формате таблица.номер_задачи: ");
+            Console.WriteLine("Какую задачу хотите посмотреть? Доступные варианты: 1.1, 1.4, 1.8, 2.3, 2.8, 2.10, 3.1. \n Введите номер в формате таблица.номер_задачи: ");
             switch(Console.ReadLine())
             {
                 case "1.1":
@@ -45,12 +45,21 @@ namespace Laba3
                     Console.ReadLine();
                     break;
                 case "2.10":
-                    Console.WriteLine("Номер 10 из таблицы 2");
-                    Console.WriteLine("Результат: " + SheetTwoNumberTen(int.Parse(Console.ReadLine())));
+                    Console.WriteLine("Номер 10 из таблицы 2, введите число m:");
+                    SheetTwoNumberTen(int.Parse(Console.ReadLine()));
+                    Console.ReadLine();
+                    break;
+                case "3.1":
+                    Console.WriteLine("Номер 1 из таблицы 3, введите число m:");
+                    int rightBorder = int.Parse(Console.ReadLine());
+                    var values = Enumerable.Range(1, rightBorder).Where(z => SheetThreeNumberOne(z));
+                    Console.WriteLine("Числа Армстронга на промежутке от 1 до " + rightBorder);
+                    foreach (var value in values) Console.WriteLine(value);
                     Console.ReadLine();
                     break;
                 default:
                     Console.WriteLine("Такой задачи нет");
+                    Console.ReadLine();
                     break;
             }
         }
@@ -81,16 +90,19 @@ namespace Laba3
 
         public static double SheetOneNumberEight(double e) 
         {
-            double sum = 1;
-            int i = 2;
-            double result = 1 / (i * i);
-            while(result >= e)
+            double sum = 0;
+            double i = 1;
+            double result = 0;
+            String numberOfDigits = e.ToString().Substring(2);
+            double Answer = Math.Round(Math.PI * Math.PI / 6, numberOfDigits.Length);
+            Console.WriteLine(result);
+            while(sum <= Answer)
             {
+                result = 1 / Math.Pow(i, 2);
                 i++;
-                Console.WriteLine(sum + "  " + Math.PI / 6);
-                result += 1 / (i * i);
-                sum += result;
-                
+                Console.WriteLine(sum + " " + Answer );
+                sum = Math.Round(sum + result, numberOfDigits.Length);
+                                                        
             }
             return sum; 
         }
@@ -128,9 +140,29 @@ namespace Laba3
             return sum;
         }
 
-        public static int SheetTwoNumberTen(int m)
+        public static void SheetTwoNumberTen(int m)
         {
-            return 0;
+            for( int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < m; j++) { 
+                    if( i*i + j*j == m)
+                    {
+                        Console.WriteLine("Это числа: " + i + ", " + j);
+                    }
+                }
+            }
+        }
+
+        public static bool SheetThreeNumberOne(int _val)
+        {
+            string valueInString = _val.ToString(); //получаем строковое представление числа
+
+            var digits = valueInString.Select(x => int.Parse(x.ToString())); //последовательность цифр в числе - каждый символ строки, приведенный к int
+
+            int n = digits.Count();//количество цифр
+
+            return _val == digits.Select(x => Math.Pow(x, n)).Sum(); //каждую цифру возводим в степень - последовательность суммируем,  проверяем на равность исходному числу и возвращаем да\нет
+
         }
 
     }
